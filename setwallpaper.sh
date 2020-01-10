fc778c7823a71565c9ce729df62e164a62b2e8f5
Jean Lourenço <jeanrafaelrodrigueslourenco@gmail.com>
2020-01-10 15:50:07 -0300

    Create setwallpaper.sh

---
+++ setwallpaper.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+###################################################################
+#Script Name	: setwallpaper.sh
+#Description	: Script para trocar o wallpaper segunda-feira ou quarta-feira.
+#Uso            : bash setwallpaper.sh
+#Date           : 09/01/2020
+#Author       	: @jeanrafaellourenco
+###################################################################
+
+DATE=$(date +%w)
+
+if [[ $DATE == 1 ]]; then
+	gsettings set org.gnome.desktop.background picture-uri "file:///home/jeanrafaellourenco/Imagens/SEGUNDA.jpg"
+elif [[ $DATE == 3 ]]; then
+	gsettings set org.gnome.desktop.background picture-uri "file:///home/jeanrafaellourenco/Imagens/QUARTA.jpg"
+else
+	echo "Nada acontece feijoada!"
+fi
+
+# Outro forma de fazer a mesma coisa
+
+# [ "$DATE" == "1" ] && gsettings set org.gnome.desktop.background picture-uri "file:///home/jeanrafaellourenco/Imagens/SEGUNDA.jpg" ||
+# [ "$DATE" == "3" ] && gsettings set org.gnome.desktop.background picture-uri "file:///home/jeanrafaellourenco/Imagens/QUARTA.jpg" || echo "Nada acontece feijoada!"
