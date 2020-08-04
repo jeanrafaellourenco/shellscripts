de5df32351cbbbc848097f0a0232a7536b0f969c
Jean Lourenço <jeanrafaelrodrigueslourenco@gmail.com>
2020-08-04 17:40:43 -0300

    Create checarSyslog.sh

---
+++ checarSyslog.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function checarSyslog() {
+	if [[ -e "/var/log/syslog" ]]; then
+		echo -e "$(ls -lha /var/log/syslog | awk '{ print $6,$7,$8,$5 }')"
+	else
+		echo -e "Arquivo de syslog não encontrado."
+		sudo touch /var/log/syslog
+		sudo chown syslog:adm /var/log/syslog
+		sudo service rsyslog restart
+		logger "Didier MISSON logger test"
+		grep "Didier MISSON logger test" /var/log/syslog
+	fi
+}
+
+checarSyslog
