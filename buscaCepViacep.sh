ea2d650705f3589ac0d7a10e9a0e5dea220e8931
Jean Lourenço <jeanrafaelrodrigueslourenco@gmail.com>
2021-05-10 23:02:29 -0300

    Update and rename buscaCep.sh to buscaCepViacep.sh

--- buscaCepViacep.sh
+++ buscaCepViacep.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-# Exemplo curl com jq:
-# curl -sf https://viacep.com.br/ws/01001000/json/ | jq
-
-# Resposta:
-# {
-# "cep": "01001-000",
-# "logradouro": "Praça da Sé",
-# "complemento": "lado ímpar",
-# "bairro": "Sé",
-# "localidade": "São Paulo",
-# "uf": "SP",
-# "unidade": "",
-# "ibge": "3550308",
-# "gia": "1004"
-# }
 
 [[ ! $(which jq 2>/dev/null) ]] && echo -e "O programa 'jq' não está instalado no momento. Você pode instalá-lo digitando: \n sudo apt install jq -y" && exit 1
 
