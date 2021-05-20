e2c752f2e78ce0b2f227e338cdc144c52194bf1e
Jean Lourenço <jeanrafaelrodrigueslourenco@gmail.com>
2021-05-20 14:26:08 -0300

    Update atualizacaoMonetaria.sh

--- atualizacaoMonetaria.sh
+++ atualizacaoMonetaria.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 # Script para checar os indices de atualização monetária
 # mensais ou anual no site do http://www.debit.com.br
-# Exemplo: ./atualizacaoMonetaria.sh 01/2019 ou
-# ./atualizacaoMonetaria.sh 2019
+# Exemplo: ./atualizacaoMonetaria.sh 01/2021 ou
+# ./atualizacaoMonetaria.sh 2021
 
-
-#url="http://www.debit.com.br/consulta30.php?indice"
 url="https://www.debit.com.br/tabelas/tabela-completa.php?indice"
 periodo=$*
 linha="-------------------------"
 
-test $(which lynx) || { echo -e "$SCRIPT: pacote 'lynx' requerido: sudo apt-get install -y lynx";  exit 1; }  && test $(which pv) || { echo -e "$SCRIPT: pacote 'pv' requerido: sudo apt-get install -y pv";  exit 1; }
-clear;
+test $(which lynx) || {
+    echo -e "$SCRIPT: pacote 'lynx' requerido: sudo apt-get install -y lynx"
+    exit 1
+} && test $(which pv) || {
+    echo -e "$SCRIPT: pacote 'pv' requerido: sudo apt-get install -y pv"
+    exit 1
+}
+clear
 echo "Atualizações do mês/ano: $periodo"
 echo "*assp corresponde ao indice TJSP."
-echo "";
+echo ""
 
 function atualizarindices_mensais() {
     indices_array=(inpc igpm aasp tjmg tjrj poupanca tjdf tjes)
 
-    for indice in "${indices_array[@]}"
-    do
-    echo "$linha";
-    echo "Atualizando $indice....." | pv -qL 15;
-
-    lynx --dump $url=$indice > $indice.txt;
-    cat $indice.txt | grep $periodo;
-
-    rm $indice.txt;
+    for indice in "${indices_array[@]}"; do
+        echo "$linha"
+        echo "Atualizando $indice....." | pv -qL 15
+        lynx --dump $url=$indice | grep $periodo
     done
+
 }
 
 atualizarindices_mensais ${@}
