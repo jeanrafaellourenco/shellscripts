4a6d53c23ca30f8cb5ef6ce64713b7673db50da8
Jean Lourenço <jeanrafaelrodrigueslourenco@gmail.com>
2021-06-20 23:13:32 -0300

    Update buscaCepCorreios.sh

--- buscaCepCorreios.sh
+++ buscaCepCorreios.sh
@@ -3,6 +3,14 @@
 [[ ! $(which jq 2>/dev/null) ]] && echo -e "O programa 'jq' não está instalado no momento. Você pode instalá-lo digitando: \n sudo apt install jq -y" && exit 1
 
 CEP=$1
-BUSCA=$(curl -sf "https://buscacepinter.correios.com.br/app/endereco/carrega-cep-endereco.php?pagina=%2Fapp%2Fendereco%2Findex.php&cepaux=&mensagem_alerta=&endereco=${CEP}&tipoCEP=ALL"  | jq)
+BUSCA=$(curl 'https://buscacepinter.correios.com.br/app/endereco/carrega-cep-endereco.php' \
+ -H 'Accept: */*' \
+ -H 'Accept-Language: pt-BR' --compressed \
+ -H 'content-type: application/x-www-form-urlencoded; charset=utf-8' \
+ -H 'Cache-Control: no-store, no-cache, must-revalidate' \
+ -H 'Origin: https://buscacepinter.correios.com.br' \
+ -H 'Connection: keep-alive' \
+ -H 'Referer: https://buscacepinter.correios.com.br/app/endereco/index.php?' \
+  --data-raw "pagina=%2Fapp%2Fendereco%2Findex.php&cepaux=&mensagem_alerta=&endereco=${CEP}&tipoCEP=ALL"  | jq)
 
 [[ ! "$BUSCA" ]] && echo -e "CEP: ${CEP} inválido ou não encontrado." || echo -e "${BUSCA}"
